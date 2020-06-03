917e1981bddf4959723933ccbd34bba39d5a85f9
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-06-03 15:36:01 -0400

    Create robot-config.h

---
+++ PD/include/robot-config.h
@@ -0,0 +1,15 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor leftDrive;
+extern motor rightDrive;
+extern controller Controller1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
