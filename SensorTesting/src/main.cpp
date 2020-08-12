570983a04b35ad66564ad77b2fe8597d0a601aa0
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-08-11 20:07:47 -0400

    Update main.cpp

--- SensorTesting/src/main.cpp
+++ SensorTesting/src/main.cpp
@@ -1,3 +1,15 @@
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// leftDrive            motor         20              
+// rightDrive           motor         11              
+// accella              inertial      6               
+// rightEncoder         encoder       C, D            
+// leftEncoder          encoder       G, H            
+// sight                vision        15              
+// ultrasonic           sonar         E, F            
+// ---- END VEXCODE CONFIGURED DEVICES ----
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
@@ -26,25 +38,45 @@ using namespace vex;
 
 int printStats()
 {
-  int counter = 0;
   while(true)
   {
-    printf("Positions: %.2f %.2f, %.2f degrees\n", leftEncoder.position(degrees), rightEncoder.position(degrees), accella.heading());
-    vex::task::sleep(500); //print the stats every half second
+    printf("Positions: %.2f / %.2f | %.2f / %.2f, %.2f degrees\n", leftEncoder.position(degrees), leftDrive.position(degrees), rightEncoder.position(degrees), rightDrive.position(degrees), accella.rotation(degrees));
+    vex::task::sleep(250); //print the stats every half second
   }
   return 1;
 };
+
+double sensorFix(double reading)
+{
+  /*if(reading > 357)
+  {
+    return 0.0;
+  }
+  else if(reading < -357)
+  {
+    return 0.0;
+  }
+  else
+  {*/
+    return reading;
+  //}
+}
 
 int main() 
 {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+  accella.calibrate();
+  while(accella.isCalibrating())
+  {
+    vex::task::sleep(250);
+  }
+  vex::task stats(printStats);
   printf("\nHello, world!");
   wait(1, seconds);
   double distanse = ultrasonic.distance(inches);
   printf("\nDistance: %.2f\n", distanse);
   vex::task::sleep(500);
-  accella.calibrate();
 
   /*if(ultrasonic.foundObject() && sight.objects[0].exists)
   {
@@ -59,13 +91,12 @@ int main()
     printf("\nI need to be reconfigured, I think.\n");
   }*/
 
-  printf("Positions: %.2f %.2f, %.2f degrees\n", leftEncoder.position(degrees), rightEncoder.position(degrees), accella.heading());
-  leftDrive.spinFor(forward, 720, degrees, false);
-  rightDrive.spinFor(forward, 720, degrees);
-  printf("Positions: %.2f %.2f, %.2f degrees\n", leftEncoder.position(degrees), rightEncoder.position(degrees), accella.heading());
-  accella.calibrate();
+  //printf("Positions: %.2f / %.2f | %.2f / %.2f, %.2f degrees\n", leftEncoder.position(degrees), leftDrive.position(degrees), rightEncoder.position(degrees), rightDrive.position(degrees), accella.rotation(degrees));
+  //leftDrive.spinFor(forward, 720, degrees, false);
+  //rightDrive.spinFor(forward, 720, degrees);
+  //printf("Positions: %.2f / %.2f | %.2f / %.2f, %.2f degrees\n", leftEncoder.position(degrees), leftDrive.position(degrees), rightEncoder.position(degrees), rightDrive.position(degrees), accella.rotation(degrees));
 
-  while((accella.heading() < 180))
+  while((accella.rotation(degrees) > -180))
   {
     leftDrive.spin(forward, -50, percent);
     rightDrive.spin(forward, 50, percent);
@@ -77,7 +108,6 @@ int main()
   while(1)
   {
     vex::task::sleep(500);
-    printf("Positions: %.2f %.2f\n", leftEncoder.position(degrees), rightEncoder.position(degrees)); 
-    printf("Positions: %.2f %.2f, %.2f degrees\n", leftEncoder.position(degrees), rightEncoder.position(degrees), accella.heading());
+    //printf("Positions: %.2f / %.2f | %.2f / %.2f, %.2f degrees\n", leftEncoder.position(degrees), leftDrive.position(degrees), rightEncoder.position(degrees), rightDrive.position(degrees), accella.rotation(degrees));
   }
 }
