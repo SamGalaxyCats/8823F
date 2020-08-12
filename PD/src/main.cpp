5b2bd44ca0dd42295e4ef502bcd92db05c7d518c
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-08-11 20:07:30 -0400

    Update main.cpp

--- PD/src/main.cpp
+++ PD/src/main.cpp
@@ -1,3 +1,27 @@
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// leftDrive            motor         20              
+// rightDrive           motor         11              
+// Controller1          controller                    
+// accella              inertial      6               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// leftDrive            motor         20              
+// rightDrive           motor         10              
+// Controller1          controller                    
+// accella              inertial      6               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// leftDrive            motor         1               
+// rightDrive           motor         10              
+// Controller1          controller                    
+// accella              inertial      6               
+// ---- END VEXCODE CONFIGURED DEVICES ----
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
@@ -22,7 +46,7 @@
 
 using namespace vex;
 
-double kP = 0.75;
+double kP = 1;
 double kD = 0;
 int pos = (leftDrive.position(degrees) + rightDrive.position(degrees)) / 2;
 int error = 180;
@@ -36,8 +60,8 @@ int printStats()
   while(true)
   {
     printf("Print statement %d.\n", counter);
-    printf("Left position | Right position | Average | Error | Previous Error | Derivative | Speed\n");
-    printf(" %f    | %f       | %d       | %d     | %d       | %d        | %f\n", leftDrive.position(degrees), rightDrive.position(degrees), pos, error, prevError, derivative, speed);
+    printf("Left position | Right position | Average | Error | Previous Error | Derivative | Speed | Heading\n");
+    printf(" %.2f     | %.2f        | %d       | %d     | %d       | %d        | %.2f | %.2f\n", leftDrive.position(degrees), rightDrive.position(degrees), pos, error, prevError, derivative, speed, accella.heading(degrees));
     counter++;
     vex::task::sleep(500); //print the stats every half second
   }
@@ -74,19 +98,51 @@ void PD(int target)
   rightDrive.stop();
 }
 
+void PDturn(int target)
+{
+  double turnKP = 4;
+  error = target;
+  int stoppingRange = 5;
+  while(dabs(error) > stoppingRange)
+  {
+    pos = accella.heading(degrees);
+    error = target - pos;
+    derivative = error - prevError;
+
+    speed = (error * turnKP) + (derivative * kD);
+    if(dabs(speed) < 20)
+    {
+      speed = 15;
+    }
+    leftDrive.spin(forward, speed, dps);
+    rightDrive.spin(forward, -speed, dps);
+    prevError = error;
+
+    wait(20, msec);
+  }
+  leftDrive.stop();
+  rightDrive.stop();
+}
+
 int main() 
 {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+  accella.calibrate();
+  while(accella.isCalibrating())
+  {
+    wait(30, msec);
+  }
   vex::task numbers(printStats);
   while(!Controller1.ButtonA.pressing())
   {
     wait(30, msec);
   }
   PD(1800); //53in 54in 54in 54in
+  //PDturn(180);
   numbers.stop();
   printf("End result: \n");
-  printf("Left position | Right position | Average | Error | Previous Error | Derivative | Speed\n");
-  printf(" %f    | %f       | %d       | %d     | %d       | %d        | %f\n", leftDrive.position(degrees), rightDrive.position(degrees), pos, error, prevError, derivative, speed);
-  Controller1.Screen.print("Done!");
+  printf("Left position | Right position | Average | Error | Previous Error | Derivative | Speed | Heading\n");
+  printf(" %.2f     | %.2f        | %d       | %d     | %d       | %d        | %.2f | %.2f\n", leftDrive.position(degrees), rightDrive.position(degrees), pos, error, prevError, derivative, speed, accella.heading(degrees));
+  Controller1.Screen.print("Done!\n");
 }
