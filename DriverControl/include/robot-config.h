085d87d0461f5af037d4bac6462510678c68dd26
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-11-05 15:57:58 -0500

    Update robot-config.h

--- DriverControl/include/robot-config.h
+++ DriverControl/include/robot-config.h
@@ -2,16 +2,27 @@ using namespace vex;
 
 extern brain Brain;
 
+using signature = vision::signature;
+
 // VEXcode devices
 extern motor fLeftDrive;
 extern motor fRightDrive;
 extern motor bLeftDrive;
 extern motor bRightDrive;
 extern controller Controller1;
-extern motor lift;
-extern motor rampIntake;
-extern motor bLinkake;
-extern motor bRinkake;
+extern motor trashHandler;
+extern motor conveyor;
+extern motor lIntake;
+extern motor rIntake;
+extern signature sight__BLUE_BALL;
+extern signature sight__RED_BALL;
+extern signature sight__SIG_3;
+extern signature sight__SIG_4;
+extern signature sight__SIG_5;
+extern signature sight__SIG_6;
+extern signature sight__SIG_7;
+extern vision sight;
+extern inertial Accella;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
