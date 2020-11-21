b1f855989006b33c62b62b4c29611696400727d7
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-11-21 11:32:52 -0500

    Update main.cpp

--- DriverControlToggleSort/src/main.cpp
+++ DriverControlToggleSort/src/main.cpp
@@ -1,3 +1,19 @@
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// fLeftDrive           motor         1               
+// fRightDrive          motor         10              
+// bLeftDrive           motor         11              
+// bRightDrive          motor         20              
+// Controller1          controller                    
+// trashHandler         motor         5               
+// conveyor             motor         4               
+// lIntake              motor         15              
+// rIntake              motor         16              
+// sight                vision        17              
+// Accella              inertial      18              
+// eyes                 optical       9               
+// ---- END VEXCODE CONFIGURED DEVICES ----
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
@@ -27,11 +43,72 @@
 
 using namespace vex;
 
+int heatCheck()
+{
+  while(true)
+  {
+    bool overheat = false;
+    int badHeat = 100;
+    if(lIntake.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("lIntake overheating/n");
+    }
+    if(rIntake.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("rIntake overheating/n");
+    }
+    if(trashHandler.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("trashHandler overheating/n");
+    }
+    if(conveyor.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("conveyor overheating/n");
+    }
+    if(bLeftDrive.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("bLeftDrive overheating/n");
+    }
+    if(bRightDrive.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("bRightDrive overheating/n");
+    }
+    if(fLeftDrive.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("fLeftDrive overheating/n");
+    }
+    if(fRightDrive.temperature() > badHeat)
+    {
+      overheat = true;
+      printf("fRightDrive overheating/n");
+    }
+    if(overheat)
+    {
+      Controller1.Screen.setCursor(1, 1);
+      Controller1.Screen.print("Motors overheating");
+    }
+    else
+    {
+      Controller1.Screen.setCursor(1, 1);
+      Controller1.Screen.print("Motors are fine :D");
+    }
+    vex::task::sleep(5000); //check the heat of the motors once every 5 seconds
+  }
+  return 1;
+};
+
 int main() 
 {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
+  vex::task stats(heatCheck);
   double speedL;
   double speedR; 
   double conveyorSpeed = 90;
