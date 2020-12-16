5ee71ddfed75cb195169f9c7d772042b753b3c03
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-12-16 17:52:26 -0500

    Create robot-config.h

---
+++ ChristmasComp/include/robot-config.h
@@ -0,0 +1,23 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor fLeftDrive;
+extern motor fRightDrive;
+extern motor bLeftDrive;
+extern motor bRightDrive;
+extern motor lIntake;
+extern motor rIntake;
+extern motor conveyor;
+extern motor trashHandler;
+extern optical eyes;
+extern distance dist;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
