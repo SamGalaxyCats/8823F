de8a924a15f216f92de26554025f50a20f704f83
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2020-12-15 20:21:33 -0500

    Update function-library.h

--- DriverControlToggleSort/include/function-library.h
+++ DriverControlToggleSort/include/function-library.h
@@ -8,4 +8,15 @@ class BackgroundTask
     bool isTrash(bool redTrash);
 };
 
+class ConveyorSystem
+{
+  public:
+    bool hasTrash(bool redTrash);
+    void takeUp(double systemSpeed);
+    void spitOut(double systemSpeed);
+    void takeDown(double systemSpeed);
+    void autoSort(bool redTrash, double objectRange, double redTreshhold, double systemSpeed);
+    void stopConveyor();
+};
+
 #endif
