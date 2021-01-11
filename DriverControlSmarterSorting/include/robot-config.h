306039399c458b19c1c2ffa63098a843fb685e04
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2021-01-11 17:19:29 -0500

    Create robot-config.h

---
+++ DriverControlSmarterSorting/include/robot-config.h
@@ -0,0 +1,26 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor fLeftDrive;
+extern motor fRightDrive;
+extern motor bLeftDrive;
+extern motor bRightDrive;
+extern controller Controller1;
+extern motor trashHandler;
+extern motor conveyor;
+extern motor lIntake;
+extern motor rIntake;
+extern inertial Accella;
+extern optical eyes;
+extern distance dist;
+extern limit clickL;
+extern limit clickR;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
