37b477c819feef7a57461c776bad33530b188176
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2021-01-11 17:20:12 -0500

    Update function-library.cpp

--- DriverControlToggleSort/src/function-library.cpp
+++ DriverControlToggleSort/src/function-library.cpp
@@ -5,43 +5,43 @@
 void BackgroundTask::heatCheck()
 {
   bool overheat = false;
-  int badHeat = 50;
-  if(lIntake.temperature() > badHeat)
+  int badHeat = 55;
+  if(lIntake.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("lIntake overheating/n");
   }
-  if(rIntake.temperature() > badHeat)
+  if(rIntake.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("rIntake overheating/n");
   }
-  if(trashHandler.temperature() > badHeat)
+  if(trashHandler.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("trashHandler overheating/n");
   }
-  if(conveyor.temperature() > badHeat)
+  if(conveyor.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("conveyor overheating/n");
   }
-  if(bLeftDrive.temperature() > badHeat)
+  if(bLeftDrive.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("bLeftDrive overheating/n");
   }
-  if(bRightDrive.temperature() > badHeat)
+  if(bRightDrive.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("bRightDrive overheating/n");
   }
-  if(fLeftDrive.temperature() > badHeat)
+  if(fLeftDrive.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("fLeftDrive overheating/n");
   }
-  if(fRightDrive.temperature() > badHeat)
+  if(fRightDrive.temperature(celsius) > badHeat)
   {
     overheat = true;
     printf("fRightDrive overheating/n");
@@ -65,32 +65,45 @@ bool BackgroundTask::isTrash(bool redTrash)
   return true;
 }
 
+void Ball::lockColor(bool wasRed)
+{
+  isLocked = true;
+  isRed = wasRed;
+}
+
+void Ball::unlockColor()
+{
+  isLocked = false;
+}
+
+void ConveyorSystem::lockConveyorBall()
+{
+  conveyorBall.lockColor(intakeBall.isRed);
+}
+
+void ConveyorSystem::lockIntakeBall()
+{
+  intakeBall.lockColor(hadRed);
+}
+
 bool ConveyorSystem::hasTrash(bool redTrash, double objectRange, double redTreshhold, bool currentState)
 {
-  if(dist.objectDistance(inches) < objectRange)
+  if(conveyorBall.isRed && redTrash)
   {
-    //eyes.setLightPower(10, percent);
-    //eyes.setLight(ledState::on);
-    if((eyes.hue() <= redTreshhold) && redTrash)
-    {
-      hadTrash = true;
-      return true;
-    }
-    else if((eyes.hue() > redTreshhold) && !redTrash)
-    {
-      hadTrash = true;
-      return true;
-    }
-    else
-    {
-      hadTrash = false;
-      return false;
-    }
+    hadTrash = true; //You have trash
+    return true;
   }
+  //If it's not red, and blue is trash
+  else if(!conveyorBall.isRed && !redTrash)
+  {
+    hadTrash = true; //You have trash
+    return true;
+  }
+  //If it's red and blue is trash or vice versa,
   else
   {
-    //eyes.setLight(ledState::off);
-    return hadTrash;
+      hadTrash = false; //There is no trash.
+      return false;
   }
 }
 
@@ -128,4 +141,34 @@ void ConveyorSystem::takeDown(double systemSpeed)
 {
   conveyor.spin(forward, -systemSpeed, percent); //outtake
   trashHandler.spin(forward, systemSpeed, percent); //trash handler helps spit out this ball
+}
+
+void ConveyorSystem::update(bool redTrash, double redTreshhold, double objectRange)
+{
+  //New autosorting logic:
+  //If the limit switches are being pressed, lock in the conveyor ball and unlock the intake ball.
+  if(clickL.pressing() || clickR.pressing())
+  {
+    lockConveyorBall();
+    intakeBall.unlockColor();
+  }
+  //If the distance sensor sees an object, lock in the intake ball again.
+  else if(dist.objectDistance(inches) <= objectRange)
+  {
+    lockIntakeBall();
+  }
+  
+  //If the optical sensor detects an object,
+  if(eyes.isNearObject())
+  {
+    //determine whether it is red.
+    if(eyes.hue() <= redTreshhold)
+    {
+      hadRed = true;
+    }
+    else
+    {
+      hadRed = false;
+    }
+  }
 }
