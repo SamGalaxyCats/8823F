29aa34465dd785b7868d3b52507bd01e45917c68
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2021-01-11 17:18:53 -0500

    Create main.cpp

---
+++ BlankWConfigs/src/main.cpp
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\sambo                                            */
+/*    Created:      Mon Jan 11 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// bLeftDrive           motor         11              
+// bRightDrive          motor         19              
+// fLeftDrive           motor         1               
+// fRightDrive          motor         9               
+// lIntake              motor         15              
+// rIntake              motor         16              
+// conveyor             motor         3               
+// trashHandler         motor         5               
+// eyes                 optical       12              
+// dist                 distance      13              
+// clicker              limit         A               
+// accella              inertial      20              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() 
+{
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
