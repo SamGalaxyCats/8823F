43f696fde6f24f508e0aef1cd7c66f802b9405ca
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2021-01-15 17:05:18 -0500

    Create robot-config.h

---
+++ SpaghettiAuton/include/robot-config.h
@@ -0,0 +1,25 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor bLeftDrive;
+extern motor bRightDrive;
+extern motor fLeftDrive;
+extern motor fRightDrive;
+extern motor lIntake;
+extern motor rIntake;
+extern motor conveyor;
+extern motor trashHandler;
+extern optical eyes;
+extern distance dist;
+extern limit clicker;
+extern inertial accella;
+extern controller Controller1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
