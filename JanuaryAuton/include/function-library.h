db0a35c1d438c3960e04c7493723306ab12dd0cb
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2021-01-23 09:50:06 -0500

    Create function-library.h

---
+++ JanuaryAuton/include/function-library.h
@@ -0,0 +1,19 @@
+#ifndef FUNCTION_LIBRARY_H
+#define FUNCTION_LIBRARY_H
+
+class Robot
+{
+  public:
+    void driveDistance(double ticks, double degreesPerSecond);
+    void driveDistance(double ticks, double degreesPerSecond, bool waitForComplete);
+    void driveDistance(double ticks, double degreesPerSecond, bool waitForComplete, bool doCoast);
+    void turnDegrees(double degrees, double degreesPerSecond);
+    void turnDegrees(double degrees, double degreesPerSecond, bool doCoast);
+    void turnLeft(double degrees, double degreesPerSecond);
+    void turnRight(double degrees, double degreesPerSecond);
+    void turnDistance(double ticksL, double speedL, double ticksR, double speedR);
+    void turnDistance(double ticksL, double speedL, double ticksR, double speedR, bool waitForComplete);
+    void turnDistance(double ticksL, double speedL, double ticksR, double speedR, bool waitForComplete, bool doCoast);
+};
+
+#endif
