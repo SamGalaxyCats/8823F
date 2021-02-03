f942316d942658c5bd13c6eec060cc8c8c9ab23f
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2021-02-02 19:37:28 -0500

    Create robot-config.cpp

---
+++ AutonSkillsCompClean/src/robot-config.cpp
@@ -0,0 +1,33 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor bLeftDrive = motor(PORT11, ratio18_1, false);
+motor bRightDrive = motor(PORT19, ratio18_1, true);
+motor fLeftDrive = motor(PORT1, ratio18_1, false);
+motor fRightDrive = motor(PORT9, ratio18_1, true);
+motor lIntake = motor(PORT15, ratio6_1, false);
+motor rIntake = motor(PORT16, ratio6_1, true);
+motor conveyor = motor(PORT3, ratio6_1, false);
+motor trashHandler = motor(PORT5, ratio6_1, true);
+distance dist = distance(PORT13);
+inertial accella = inertial(PORT18);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
